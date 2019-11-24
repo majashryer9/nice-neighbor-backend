cb86b75d594b98b99334572a1aa734ecb23b9cd0
Maja Shryer <majashryer@gmail.com>
2019-11-24 10:05:11 -0600

    Initial application setup.

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+// Temporarily allow all CORS
+app.use(cors());
+app.use(bodyParser.json());
+
+const port = process.env.PORT ? process.env.PORT : 8080;
+app.listen(port, () => {
+    console.log(`Application is listening on port ${port}`);
+});
